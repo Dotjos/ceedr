a7cf6cadf46631771b0946dd5cf8662c40e23d19
Jose <oladotjos@gmail.com>
2026-02-05 15:59:02 +0100

    feat: Integrate Poppins font, refine typography, and enhance responsiveness across the landing page.

--- src/app/Components.tsx
+++ src/app/Components.tsx
@@ -1,68 +1,118 @@
 import Image from "next/image";
 import Link from "next/link";
 
+export function Navbar() {
+  return (
+    <nav className="bg-white flex justify-between items-center p-3 w-full">
+      <Link href="">
+        <div className=" h-6 w-15 relative md:h-10 md:w-25 md:ml-10">
+          <Image
+            src="/ceedrLogo.png"
+            alt="Ceedr Logo"
+            fill
+            priority
+            className="md:h-12 border object-contain"
+          />
+        </div>
+      </Link>
 
-export function Navbar(){
-   return <nav className="bg-white flex justify-between items-center p-3 w-full">
-          <Link href="">
-          <div className=" h-6 w-15 relative md:h-10 md:w-25 md:ml-10">
-            <Image src="/ceedrLogo.png" alt="Ceedr Logo" fill priority className="md:h-12 border"/>
-          </div>
-          </Link>
-          
-            <Image src="/Menu.png" alt="Menu Logo" width={20} height={50} className="md:hidden" />
-            
-            <nav className="md:flex md:w-3/5 text-ceedr-500 hidden md:justify-around md:text-sm md:font-medium">
-                <ul className="flex space-x-2 items-center justify-center md:space-x-10">
-                 <li>
-                   <a href="#services" className="hover:underline">Services</a>
-                 </li>
-                 <li>
-                   <a href="#testimonials" className="hover:underline">Testimonials</a>
-                 </li>
-                 <li>
-                  <a href="#contact" className="hover:underline">Contact</a>
-                 </li>
-                </ul>
+      <Image
+        src="/Menu.png"
+        alt="Menu Logo"
+        width={20}
+        height={50}
+        className="md:hidden object-contain"
+      />
 
-                <button className="border flex text-xs items-center space-x-4 border-ceedr-500 text-ceedr-500 p-3">
-                    <Image src="/whatsapp500.png" alt="WhatsApp Icon" width={20} height={10} className="inline-block ml-2" />
-                    <p>Chat With Us on WhatsApp</p>
-                    </button>
+      <div className="md:flex md:w-3/5 text-ceedr-500 hidden md:justify-around md:text-sm md:font-medium">
+        <ul className="flex space-x-2 items-center font-black justify-center md:space-x-10">
+          <li>
+            <a href="#services" className="hover:underline">
+              Services
+            </a>
+          </li>
+          <li>
+            <a href="#testimonials" className="hover:underline">
+              Testimonials
+            </a>
+          </li>
+          <li>
+            <a href="#contact" className="hover:underline">
+              Contact
+            </a>
+          </li>
+        </ul>
 
-      </nav>
-         </nav>
+        <button className="border font-black flex text-xs items-center space-x-4 border-ceedr-500 text-ceedr-500 p-3">
+          <Image
+            src="/whatsapp500.png"
+            alt="WhatsApp Icon"
+            width={20}
+            height={10}
+            className="inline-block ml-2 object-contain"
+          />
+          <p>Chat With Us on WhatsApp</p>
+        </button>
+      </div>
+    </nav>
+  );
 }
 
-interface ButtonProps{
-    text:string
-    textColor:string
-    btnImage:boolean
-    backgroundColor:string
-    className?:string
+interface ButtonProps {
+  text: string;
+  textColor: string;
+  btnImage: boolean;
+  backgroundColor: string;
+  className?: string;
 }
 
-export function Button({text,textColor,btnImage,backgroundColor,className}: ButtonProps){
-    return  <button className={`bg-${backgroundColor} md:text-xl text-center text-${textColor} flex items-center justify-center px-5 w-6/7 space-x-2 py-3 ${className}`}>
-       {btnImage? <Image className="" src="/white-whatsApp-icon.png" alt="button image" width={20} height={10}/>:""}
-       <span className="inline-block text-[9px]">{text}</span>
+export function Button({
+  text,
+  textColor,
+  btnImage,
+  backgroundColor,
+  className,
+}: ButtonProps) {
+  return (
+    <button
+      className={`bg-${backgroundColor} py-2 px-4 md:text-xl text-center text-${textColor} flex space-x-3 items-center justify-center ${className}`}
+    >
+      {btnImage && (
+        <Image
+          className="object-contain"
+          src="/white-whatsApp-icon.png"
+          alt="button image"
+          width={20}
+          height={10}
+        />
+      )}
+      <span className="text-[9px] mt-1">{text}</span>
     </button>
+  );
 }
 
 interface contactCardProps {
-    imgPath:string
-    smText:string
-    address:string
+  imgPath: string;
+  smText: string;
+  address: string;
 }
 
-export function ContactCard({imgPath,smText,address}:contactCardProps){
-    return <div className="bg-white p-3 border flex text-xs w-full space-x-3">
-        <Image src={imgPath} alt="Ceedr Logo" width={40} height={15} className="bg-ceedr-500 p-2 border" />
-        <div className="flex flex-col justify-center">
-            <h1>{smText}</h1>
-            <h1>{address}</h1>
-        </div>
+export function ContactCard({ imgPath, smText, address }: contactCardProps) {
+  return (
+    <div className="bg-white p-3 border flex text-xs w-full space-x-3">
+      <Image
+        src={imgPath}
+        alt="Ceedr Logo"
+        width={40}
+        height={15}
+        className="bg-ceedr-500 p-2 border object-contain"
+      />
+      <div className="flex flex-col justify-center">
+        <h1>{smText}</h1>
+        <h1>{address}</h1>
+      </div>
     </div>
+  );
 }
 
-export function Footer(){}
+export function Footer() {}
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,13 @@
 import type { Metadata } from "next";
+import { Poppins } from "next/font/google";
 import "./globals.css";
 import bonche from "./fonts/boncheConf";
 
+const poppins = Poppins({
+  subsets: ["latin"],
+  weight: ["100", "200", "300", "400", "500", "600", "700", "800", "900"],
+  variable: "--font-poppins",
+});
 
 export const metadata: Metadata = {
   title: "Ceedr landing page",
@@ -14,12 +20,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={bonche.className} >
-      <body
-        className={``}
-      >
-        {children}
-      </body>
+    <html lang="en" className={`${bonche.className} ${poppins.variable}`}>
+      <body className={``}>{children}</body>
     </html>
   );
 }
